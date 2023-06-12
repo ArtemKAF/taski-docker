0c41e6aa8d7dfa66f745837e1f7828c3dd16fe1b
Kozin <grot_artem@inbox.ru>
2023-06-12 20:42:03 +0300

    Fixed code pep8

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -8,7 +8,7 @@ class TaskiAPITestCase(TestCase):
     def setUp(self) -> None:
         self.guest_client = Client()
         return super().setUp()
-    
+
     def test_list_exists(self):
         """Проверка доступности задач."""
         responce = self.guest_client.get('/api/tasks/')
@@ -16,7 +16,7 @@ class TaskiAPITestCase(TestCase):
 
     def test_task_creation(self):
         """Проверка создания задачи."""
-        data={'title': 'Test', 'description': 'Test'}
+        data = {'title': 'Test', 'description': 'Test'}
         responce = self.guest_client.post('/api/tasks/', data)
         self.assertEqual(responce.status_code, HTTPStatus.CREATED)
         self.assertTrue(models.Task.objects.filter(title='Test').exists())
